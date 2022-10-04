bcf40bf3371bcea968e2022e69b19dd2761deee5
Elise <eliseraney@gmail.com>
2022-10-03 19:08:09 -0600

    uploaded index & src files

---
+++ src/script.js
@@ -0,0 +1,81 @@
+function search(city) {
+  let apiKey = "701f06352d61835bc4fc894e7b084629";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+function handlePosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findCurrentCity);
+}
+
+function findCurrentCity(position) {
+  let apiKey = "701f06352d61835bc4fc894e7b084629";
+  let units = "metric";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function displayWeatherConditions(response) {
+  let cityName = response.data.name;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityName}`;
+
+  let condition = response.data.weather[0].main;
+  let weatherDescription = document.querySelector("p");
+  weatherDescription.innerHTML = `${condition}`;
+
+  let temperature = Math.round(response.data.main.temp);
+  let highTemp = document.querySelector(".high");
+  highTemp.innerHTML = `${temperature}°C`;
+
+  let speed = Math.round(response.data.wind.speed);
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = `${speed}m/s`;
+
+  let humidity = response.data.main.humidity;
+  let humidityPercent = document.querySelector("#humidity");
+  humidityPercent.innerHTML = `${humidity}%`;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let hours = now.getHours();
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDate = document.querySelector("#date-time");
+currentDate.innerHTML = `${day}  ${hours}:${minutes}`;
+
+let currentCityButton = document.querySelector("#current-city-button");
+currentCityButton.addEventListener("click", handlePosition);
+
+search("Houston");
